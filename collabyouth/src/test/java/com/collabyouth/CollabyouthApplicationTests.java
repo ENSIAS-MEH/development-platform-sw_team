9dbd4f916d57ee62e573048774a0c9b171870b47
warawafae <wafaebarara@gmail.com>
2026-04-20 00:06:31 +0100

    Initialisation du project

---
+++ collabyouth/src/test/java/com/collabyouth/CollabyouthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabyouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabyouthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
